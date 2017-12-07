91764844dc0364c4515796635444dcdde7f73e98
Alexgta <9720560@gmail.com>
2017-12-07 14:32:17 +0300

    3.2. Максимум из двух чисел + test. НЕ вижу сообщения об ошибке, т.к. второй тест специально сделан НЕ правильно Конечно через Idea.

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,13 @@
+package ru.job4j.max;
+/**
+ * Класс Mac c методом max для вычислния большего из двух.
+ * @author   AVasiliev
+ * @since    07.12.2017
+ * @version  1.0.0
+ */
+
+public class Max {
+   public int max(int first, int second) {
+      return (first >= second) ? first : second;
+   }
+}
---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Alex Vasiliev (parsentev@yandex.ru)
+ * @version $Id$
+ * @since 07/12/2017
+ */
+public class MaxTest {
+
+   @Test
+   public void whenFirstLessThenSecond() {
+      Max maxim = new Max();
+      int result = maxim.max(1, 2);
+      assertThat(result, is(2));
+   }
+
+   @Test
+   public void whenFirstMoreThenSecond() {
+      Max maxim = new Max();
+      int result = maxim.max(5, 2);
+      // Ниже специально ошибка что бы поймать сообщение об ошибке, которое НЕ выходит.
+      // Понятно что должно быть assertThat(result, is(5));
+      assertThat(result, is(0));
+   }
+}
