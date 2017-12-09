4cfddf962a66a56ce32df9a10deab31080ac9ed1
Alexgta <9720560@gmail.com>
2017-12-09 21:24:25 +0300

    5.0. Заполнить массив степенями чисел от 1 до N

--- chapter_001/src/main/java/ru/job4j/array/Square.java
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -3,19 +3,19 @@ package ru.job4j.array;
 /**
  * Первоый тестовый класс для проверки настроек.
  * @author   AVasiliev
- * @since    08.12.2017
+ * @since    09.12.2017
  * @version  1.0.0
  */
 
 public class Square {
    /**
-    * Метод должен вычислять сумму четныx чисел в диапазоне от start до finish;    *
+    * Метод должен строить массив из квадратов от 1 до N;    *
     */
    public int[] calculate(int bound) {
       int[] rst = new int[bound];
-      // заполнить массив через цикл элементами от 1 до bound возведенные в квадрат
+      //  заполнить массив через цикл элементами от 1 до bound возведенные в квадрат
       for (int i = 0 ; i < bound; i++) {
-         rst[i] = (int) Math.pow(i + 1, 2);
+         rst[i] = (int) Math.pow(i + 1, 2) ;
       }
       return rst;
    }
