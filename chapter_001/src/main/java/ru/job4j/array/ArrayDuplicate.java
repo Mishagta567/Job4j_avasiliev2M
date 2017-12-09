49b4aa7f23739feb38ef32dd0a7250dfb46f51f6
Alexgta <9720560@gmail.com>
2017-12-10 00:10:36 +0300

    5.5. Удаление дубликатов в массиве.

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,50 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+/**
+ * Первоый тестовый класс для проверки настроек.
+ * @author   AVasiliev
+ * @since    09.12.2017
+ * @version  1.0.0
+ */
+
+public class ArrayDuplicate {
+   /**
+    * Метод должен находить дубликаты и удалять их
+    */
+   public String[] remove(String[] array) {
+      // пройдемся
+      int lgth = array.length - 1;
+      int dublNumbers = 0;
+      String tmp;
+      for (int n = 0; n < lgth ; n++) {
+         for (int m = n + 1; m <= lgth; m++) {
+            // Проверяем только если мы уже не залезли в "удаленное"
+            if ((n < lgth - dublNumbers)
+                && (m < lgth - dublNumbers)) {
+               // только тогда выполняем проверки
+               if (array[n].equals(array[m])) {
+                  // убираем дубль в конец
+                  tmp = array[lgth - dublNumbers];
+                  array[lgth - dublNumbers] = array[m];
+                  array[m] = tmp;
+                  dublNumbers++;
+                  // проверить бы еще раз эту же позицию, т.к. могли заменить на такой же дубль
+                  m--;
+              }
+            }
+         }
+      }
+      array = Arrays.copyOf(array, lgth + 1 - dublNumbers );
+      return array;
+   }
+
+   public static void main(String[] arg) {
+      ArrayDuplicate ar = new ArrayDuplicate();
+      String[] start = {"Привет", "Мир", "Привет", "XXXXX", "Мир"};
+      String[] rslt = ar.remove(start);
+      //System.out.println(rslt);
+   }
+
+}
