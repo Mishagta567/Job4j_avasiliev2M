8d49b6eac23ce9d1201d8bddc3a08a3c3f4a241e
Alexgta <9720560@gmail.com>
2018-01-04 18:14:18 +0300

    5.1.2. Создать итератор четные числа [#150]

---
+++ chapter_002/src/main/java/ru/job4j/lvl2junior/iterator/EvenNumbers.java
@@ -0,0 +1,51 @@
+package ru.job4j.lvl2junior.iterator;
+
+
+import java.util.Iterator;
+
+/**
+ * Движение итератора только по четным элементам массива
+ * @author   A_Vasiliev
+ * @since    04.01.2018
+ * @version  1.0.0
+ */
+
+public class EvenNumbers implements Iterator{
+    private int[] evenNumbers;
+    private int indx = 0;
+
+    public EvenNumbers(int[] array) {
+        this.evenNumbers = array;
+    }
+
+    // возвращает индекс следующего четного элемнта, начиная с startFrom (включая)
+    private int hasNextEvan(int startFRom) {
+        int rslt = -1;
+        if (indx != -1) {
+            // для начала нужно пройтись по всем оставшимся ячейкам что бы убедиться - если еще четные числа
+            for (int i = startFRom; i < this.evenNumbers.length; i++) {
+                if (this.evenNumbers[i] % 2 == 0) {
+                    rslt = i;
+                    break; // нет смысла искать еще четные. Можем выйти из цикла.
+                }
+            }
+        }
+        return rslt;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return (indx < this.evenNumbers.length && hasNextEvan(indx) != -1);
+    }
+
+    @Override
+    public Object next() {
+        // выбросить ошибку можно по сравнению: if (rslt == -1), но что делать в этом случае - не понятно.
+        indx = hasNextEvan(indx);
+        int rslt = this.evenNumbers[hasNextEvan(indx)];
+        indx = hasNextEvan(indx + 1);
+        return rslt;
+    }
+
+}
+
---
+++ chapter_002/src/test/java/ru/job4j/lvl2junior/iterator/EvenNumbersTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.lvl2junior.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class EvenNumbersTest {
+   private Iterator<Integer> it;
+
+   @Before
+   public void setUp(){
+      it = new EvenNumbers(new int[]{11, 12, 14, 15, 16, 17, 19, 21});
+   }
+
+   @Test //(expected = NoSuchElementException.class)
+   public void shouldReturnEvenNumbersSequentially () {
+      assertThat(it.hasNext(), is(true));
+      assertThat(it.next(), is(12));
+      assertThat(it.hasNext(), is(true));
+      assertThat(it.next(), is(14));
+      assertThat(it.hasNext(), is(true));
+      assertThat(it.next(), is(16));
+      assertThat(it.hasNext(), is(false));
+//      it.next();
+   }
+
+   @Test
+   public void sequentialHasNextInvocationDoesntAffectRetrievalOrder () {
+      assertThat(it.hasNext(), is(true));
+      assertThat(it.hasNext(), is(true));
+      assertThat(it.next(), is(12));
+      assertThat(it.next(), is(14));
+      assertThat(it.next(), is(16));
+   }
+
+   @Test
+   public void  shouldReturnFalseIfNoAnyEvenNumbers(){
+      it = new EvenNumbers(new int[]{1});
+      assertThat(it.hasNext(), is(false));
+   }
+
+}
