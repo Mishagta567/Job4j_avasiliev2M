9ff75eb26c5d32deecbe4c3ff7c103ea0706c1b8
Alexgta <9720560@gmail.com>
2018-01-05 17:58:02 +0300

    5.2.1. Реализовать SimpleArray<T> [#156]

---
+++ chapter_002/src/main/java/ru/job4j/lvl2junior/generic/SimpleArray.java
@@ -0,0 +1,53 @@
+package ru.job4j.lvl2junior.generic;
+
+
+
+/**
+ * Простой массив (или должна была быть коллекция?)
+ * @author   A_Vasiliev
+ * @since    05.01.2018
+ * @version  1.0.0
+ */
+
+public class SimpleArray<T>  {
+    private Object[] objects;
+    private int index = 0;
+
+    public SimpleArray(int size) {
+        this.objects = new Object[size];
+    }
+
+    public void add(T value) {
+        if (this.index < objects.length) {
+            this.objects[index++] = value;
+        }
+    }
+
+    public void update(T value, int position) {
+        if (position <= this.index) {
+            this.objects[position] = value;
+        }
+    }
+
+    public void delete(int position) {
+        if (position <= this.index && position >= 0) {
+            for (int loopIndex = position; loopIndex < this.index; loopIndex++) {
+                this.objects[loopIndex++] = this.objects[loopIndex];  // перемещаем всю цепочку справа на лево до ячейки удаления
+            }
+            objects[this.index--] = null; // как бы удаляем последний объект и уменьшаем index на 1
+        }
+    }
+
+    public T get(int position) {
+        return (T) this.objects[position];
+    }
+
+    public static void main(String[] arg) {
+        SimpleArray<String> sa = new SimpleArray(10);
+        sa.add("1-1");
+        sa.add("2-2");
+        System.out.println(sa.get(0));
+        System.out.println(sa.get(1));
+    }
+
+}
---
+++ chapter_002/src/test/java/ru/job4j/lvl2junior/iterator/SimpleArrayTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.lvl2junior.iterator;
+
+import org.junit.Test;
+import ru.job4j.lvl2junior.generic.SimpleArray;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+public class SimpleArrayTest {
+
+   SimpleArray<String> smplA = new SimpleArray<String>(10);
+
+   @Test
+   public void checkAllFunctionsHere() throws Exception {
+      smplA.add("A1");
+      smplA.add("B2");
+      String[] expect = new String[10];
+      expect[0] = "A1";
+      expect[1] = "B2";
+      assertThat(smplA.get(1), is("B2"));
+      //assertThat(smplA, is(expect));   // не понятно почему так не работает
+      smplA.update("C3", 1);
+      assertThat(smplA.get(1), is("C3"));
+      smplA.add("D4");
+      smplA.delete(1);
+      assertThat(smplA.get(1), is("D4"));
+   }
+
+}
