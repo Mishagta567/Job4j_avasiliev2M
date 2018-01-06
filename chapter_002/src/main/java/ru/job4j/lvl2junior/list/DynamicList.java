b1013fb425ee9f21785fc4dba32ba0eedf64b063
Alexgta <9720560@gmail.com>
2018-01-06 22:20:42 +0300

    5.3.1. Создать динамический список на базе массива. [#158]

---
+++ chapter_002/src/main/java/ru/job4j/lvl2junior/list/DynamicList.java
@@ -0,0 +1,112 @@
+package ru.job4j.lvl2junior.list;
+
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Простой массив (или должна была быть коллекция?)
+ * @author   A_Vasiliev
+ * @since    05.01.2018
+ * @version  1.0.0
+ */
+
+public class DynamicList<T> implements Iterable<T> {
+    private Object[] objects;
+    private int index = 0;
+    private int modCount = 0;
+
+    public DynamicList() {
+        this.objects = new Object[2];      // лучше конечно делать 100, но для тестов сделаем пока так
+    }
+
+    public void add(T value) {              // по аналогии можно создать уменьшение размера "листа".
+        if (this.index >= objects.length - 1) {
+            this.sizeIncrease();
+            //System.out.println("Size increase work");
+        }
+        this.objects[index++] = value;
+        this.modCount++;
+    }
+
+    public void update(T value, int position) {
+        if (position <= this.index) {
+            this.objects[position] = value;
+            this.modCount++;
+        }
+    }
+
+    public void delete(int position) {
+        if (position <= this.index && position >= 0) {
+            for (int loopIndex = position; loopIndex < this.index; loopIndex++) {
+                this.objects[loopIndex++] = this.objects[loopIndex];  // перемещаем всю цепочку справа на лево до ячейки удаления
+            }
+            objects[this.index--] = null; // как бы удаляем последний объект и уменьшаем index на 1
+            this.modCount++;
+        }
+    }
+
+    public T get(int position) {
+        return (T) this.objects[position];
+    }
+
+    private void sizeIncrease() {
+        Object[] tempObject = new Object[objects.length * 2];
+        for (int indx = 0; indx <= this.index; indx++) {
+            tempObject[indx] = objects[indx];
+        }
+        objects = tempObject;
+    }
+
+    public static void main(String[] arg) {
+        DynamicList<String> sa = new DynamicList<String>();
+        System.out.println("index: " + sa.index + ", length: " + sa.objects.length);
+        sa.add("1-1");
+        sa.add("2-2");
+        sa.add("3-3");
+        sa.add("4-4");
+        sa.add("5-5");
+        System.out.println(sa.get(0));
+        System.out.println(sa.get(4));
+
+        //ArrayIterator it = new ArrayIterator();
+
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ArrayIterator();
+    }
+
+    class ArrayIterator implements Iterator<T> {
+        int current = 0;  // the current element we are looking at
+        int expectedModCount = DynamicList.this.modCount;
+
+        // return whether or not there are more elements in the array that
+        // have not been iterated over.
+        @Override
+        public boolean hasNext() {
+            boolean result = false;
+            if (this.current < DynamicList.this.index) {
+                result = true;
+            }
+            return result;
+        }
+
+        // return the next element of the iteration and move the current
+        // index to the element after that.
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            if (this.expectedModCount != DynamicList.this.modCount) {
+                throw new ConcurrentModificationException();
+            }
+            return (T) DynamicList.this.objects[this.current++];
+        }
+    }
+
+
+}
---
+++ chapter_002/src/test/java/ru/job4j/lvl2junior/list/DynamicListTest.java
@@ -0,0 +1,69 @@
+package ru.job4j.lvl2junior.list;
+
+import org.junit.Before;
+import org.junit.Test;
+import ru.job4j.lvl2junior.iterator.EvenNumbers;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class DynamicListTest {
+   private Iterator<Integer> it;
+
+   @Before
+   public void setUp() {
+      DynamicList<String> dl = new DynamicList<String>();
+   }
+
+
+   @Test
+   public void add() throws Exception {
+      setUp();
+      DynamicList<String> dl = new DynamicList<String>();   //    Вопрос: почему тест не видит уже созданного dl выше?
+      dl.add("A1");
+      assertThat(dl.get(0), is("A1"));
+      dl.add("B2");
+      assertThat(dl.get(1), is("B2"));
+      dl.add("C3");
+      assertThat(dl.get(2), is("C3"));
+   }
+
+   @Test
+   public void update() throws Exception {
+      DynamicList<String> dl = new DynamicList<String>();   //    Вопрос: почему тест не видит уже созданного dl выше?
+      dl.add("A1");
+      assertThat(dl.get(0), is("A1"));
+      dl.update("B2", 0);
+      assertThat(dl.get(0), is("B2"));
+   }
+
+   @Test
+   public void delete() throws Exception {
+      DynamicList<String> dl = new DynamicList<String>();   //    Вопрос: почему тест не видит уже созданного dl выше?
+      dl.add("A1");
+      dl.add("B2");
+      assertThat(dl.get(0), is("A1"));
+      dl.delete(0);
+      assertThat(dl.get(0), is("B2"));
+   }
+
+   @Test
+   public void iteratorWorkCheck() throws ConcurrentModificationException {
+      String allData = new String();
+      DynamicList<String> dl = new DynamicList<String>();   //    Вопрос: почему тест не видит уже созданного dl выше?
+      dl.add("A1");
+      dl.add("B2");
+      dl.add("C3");
+      Iterator<String> it = dl.iterator();
+      while (it.hasNext()) {
+         allData = allData + it.next();
+         // dl.add("E4");     // генерирует ConcurrentModificationException
+      }
+      assertThat(allData, is("A1B2C3"));
+   }
+
+}
