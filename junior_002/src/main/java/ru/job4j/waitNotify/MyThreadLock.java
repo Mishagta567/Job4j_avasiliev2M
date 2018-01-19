a4cfacbf3f3f0bf6961180afa5f0ef3f2d0cd40d
Alexgta <9720560@gmail.com>
2018-01-19 17:50:08 +0300

    3. Реализовать собственный механизм блокировок Lock

---
+++ junior_002/src/main/java/ru/job4j/waitNotify/MyThreadLock.java
@@ -0,0 +1,54 @@
+package ru.job4j.waitNotify;
+
+/**
+ * Реализовать собственный механизм блокировок Lock. [#1100]
+ *
+ * @author   A_Vasiliev
+ * @since    19.01.2018
+ * @version  1.0.0
+ */
+
+
+public class MyThreadLock {
+	private String lockString;
+	private Boolean objectLocked;
+	private long lockedThread;
+
+	public void MyLock(String value) {
+		this.lockString = value;
+		this.objectLocked = false;
+	}
+
+	public void DoSomthing(String value) {
+		synchronized (objectLocked) {
+			System.out.println("DoSomthing");
+			this.lockString = value;
+			this.objectLocked.notify();
+		}
+	}
+
+	public void lock() throws InterruptedException {
+		synchronized (objectLocked) {
+			while (this.objectLocked) {
+				objectLocked.wait();
+			}
+			objectLocked = true;
+			objectLocked.notify();
+			lockedThread = Thread.currentThread().getId();
+		}
+	}
+
+	public void unLock() throws InterruptedException {
+		synchronized (objectLocked) {
+			// Mы что-то делаем только если это наш поток залочил наш объект
+			if (lockedThread == Thread.currentThread().getId()) {
+				while (this.objectLocked) {
+					objectLocked.wait();
+				}
+				objectLocked = false;
+				objectLocked.notify();
+				lockedThread = 0;
+			}
+		}
+	}
+}
