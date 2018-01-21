48ae14f347a94aca1a8ce180de1556a5aa9b65c3
Alexgta <9720560@gmail.com>
2018-01-21 19:14:28 +0300

    5.2.2. Реализовать Store<T extends Base> [#157]

--- chapter_002/src/main/java/ru/job4j/junior001/generic/UserStore.java
+++ chapter_002/src/main/java/ru/job4j/junior001/generic/UserStore.java
@@ -1,54 +1,71 @@
 package ru.job4j.junior001.generic;
 
-public class UserStore<T> {
-   //private AbstractStore<User>[] storeArray = new AbstractStore<User>(10);
+/**
+ * Реализовать Store
+ * @author   A_Vasiliev
+ * @since    20.01.2018
+ * @version  1.0.0
+ */
 
-   private Object[] objects;
-   private int index = 0;
+
+//public class UserStore extends AbstractStore<User> implements Store<User> {
+public class UserStore extends AbstractStore<User> {
+
+	/**		Это все НЕ НУЖНО вроде. Для работы достаточно того, что есть в AbstractStore
+	private String name;
+   private Object[] uStore;
+   private int index = 0;  */
 
    public UserStore(int size) {
-      this.objects = new Object[size];
+
+		 //  intelJ заставила написать меня след. строчку, но я не понимаю: Разве мы можем вызывать конструктор / создавать объект абстр. класса?
+   	super(size);  // Почему не могу сделать AbstractStore(size) ?
+
+   	///uStore = new Object[size];
    }
 
-   public void add(T value) {
-         this.objects[index++] = value;
+   /**
+   @Override
+	public void add(User value) {
+         this.uStore[index++] = value;
    }
 
-   public boolean update(String id, Base model) {
+   @Override
+	public boolean update(String id, User value) {
       int position = new Integer(id);
       boolean result = false;
       if (position <= this.index) {
-         this.objects[position] = model;
+         this.uStore[position] = value;
          result = true;
       }
       return result;
-   }
+   } //
 
-   public boolean delete(String id) {
+
+	@Override
+	public boolean delete(String id) {
       int position = new Integer(id);
       boolean result = false;
       if (position <= this.index && position >= 0) {
          for (int loopIndex = position; loopIndex < this.index; loopIndex++) {
-            this.objects[loopIndex++] = this.objects[loopIndex];  // перемещаем  всю  цепочку справа на лево до ячейки удаления
+            this.uStore[loopIndex++] = this.uStore[loopIndex];  // перемещаем  всю  цепочку справа на лево до ячейки удаления
          }
-         objects[this.index--] = null; // как бы удаляем последний объект и уменьшаем index на 1
+         uStore[this.index--] = null; // как бы удаляем последний объект и уменьшаем index на 1
          result = true;
       }
       return result;
-   }
+   } //  */
 
-   public static void main(String[] arg) {
-      User ivan = new User("U1");
-      Role admin = new Role("R1");
-      UserStore<User> userSt = new UserStore<User>(10);
+   public static void main(String[] args) {
+		System.out.println("Yo");
+		UserStore usr = new UserStore(10);
+		usr.add(new User("50"));
+		usr.add(new User("51"));
+		//System.out.println(usr.uStore[1].toString());
+		System.out.println(usr.get("1").getId());
 
-      userSt.add(ivan);
-      //userSt.add(admin);       // Не могу выполнить, что и требовалось.
 
-      SimpleArray<User> userSt2 = new SimpleArray<User>(10);
-      userSt2.add(ivan);
-      //userSt2.add(admin);      // Не могу выполнить, что и требовалось.
-      System.out.print("Yo");
-   }
+	}
+
 
 }
