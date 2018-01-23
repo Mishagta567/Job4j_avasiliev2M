bb88c0a11e0a4e31f1f30369fd19a23a26005bd1
Alexgta <9720560@gmail.com>
2018-01-23 19:20:30 +0300

    2. Реализовать ThreadPool [#1099] - v2

--- junior_002/src/main/java/ru/job4j/waitNotify/ThreadPool.java
+++ junior_002/src/main/java/ru/job4j/waitNotify/ThreadPool.java
@@ -15,14 +15,15 @@ import java.util.Queue;
 
 
 public class ThreadPool<W extends Runnable> {
-	private static Queue currentJobList = new LinkedList();
-	private static int count = 0;
+	private Queue currentJobList = new LinkedList();
+	private int count = 0;
 	private final int maxTreadCount = Runtime.getRuntime().availableProcessors();
+	private Boolean stopWork = false;
 
 
-	public static void workOne() throws InterruptedException {
+	public void workOne() throws InterruptedException {
 			for (int indx = 0; indx < 5; indx++) {
-			System.out.printf("work-%S, indx: %s%n", ThreadPool.count++, indx);
+			System.out.printf("work-%S, indx: %s%n", count++, indx);
 			Thread.sleep(1000);
 		}
 	}
@@ -34,16 +35,16 @@ public class ThreadPool<W extends Runnable> {
 		}
 	}
 
+	public void stop() {
+		synchronized (this.currentJobList) {
+			stopWork = true;
+			stopWork.notify();
+		}
+	}
 
-	public void main(String args[]) throws InterruptedException {
-		ThreadPool tp = new ThreadPool();
-		//tp.add(W tp.workOne());
-		//tp.add(tp.workOne());
-
-		// А кто нам мешает сделать loop и запустить столько нитей, сколько у нас maxTreadCount ?
+	public void work() {
 
 		for (int threadIndx = 1; threadIndx <= maxTreadCount; threadIndx++) {
-
 			// Каждый Thread будет либо спать, либо выполнять какую-то работу.
 			new Thread() {
 				@Override
@@ -53,21 +54,21 @@ public class ThreadPool<W extends Runnable> {
 					// Хочется запустить вечный цикл в котором нити будут ждать появления новых задач
 					// Понятно что в идеале лучше сделать какой-то параметр, после обнавления которого нити закрываются,
 					// но сейчас это не принципиально.
-					while (2 > 1) {
+					while (stopWork) synchronized (stopWork){
 
 						// этот цикл - запускаем work до тех пор пока они есть в листе работ, или ложиться спать
-						while (tp.currentJobList.size() == 0) synchronized (tp.currentJobList) {
+						while (currentJobList.size() == 0) synchronized (currentJobList) {
 							try {
-								tp.currentJobList.wait();
+								currentJobList.wait();
 							} catch (InterruptedException e) {
 								e.printStackTrace();
 							}
 						}
 
-						while (tp.currentJobList.size() > 0) synchronized (tp.currentJobList) {
+						while (currentJobList.size() > 0 && !stopWork) synchronized (currentJobList) {
 							//
-							newWork = (W) tp.currentJobList.remove();
-							tp.currentJobList.notify();
+							newWork = (W) currentJobList.remove();
+							currentJobList.notify();
 							// Я исхожу из того что цикл НЕ будет повторяться пока не будет выполнен полностью код в данной работе
 							newWork.run();
 						}
@@ -77,5 +78,10 @@ public class ThreadPool<W extends Runnable> {
 		}
 	}
 
+	public static void main(String[] args) {
+		ThreadPool pool = new ThreadPool();
+		//pool.add(new Work());
+		pool.stop();
+	}
 
 }
