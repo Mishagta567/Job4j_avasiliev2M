2091e86bb8b897195db68045f5e4aec6defd85f2
Alexgta <9720560@gmail.com>
2018-01-28 16:41:52 +0300

    3. Собственная реализация Binary search tree [#1714]: add modCount

--- chapter_002/src/main/java/ru/job4j/junior001/tree/TreeNodeBST.java
+++ chapter_002/src/main/java/ru/job4j/junior001/tree/TreeNodeBST.java
@@ -13,11 +13,17 @@ import java.util.*;
  */
 public class TreeNodeBST<T extends Comparable<T>> implements Iterable<T> {
 	private final NodeBST root = new NodeBST(null);
+	private int modCount = 0;
+
+	public int getModCount() {
+	   return this.modCount;
+   }
 
 	private final Comparator<T> nullSafeComparator = Comparator.nullsFirst(Comparator.naturalOrder());
 
 	public void add(T value) {
 		this.root.addChild(value);
+      modCount++;
 	}
 
 	private class NodeBST {
@@ -48,8 +54,8 @@ public class TreeNodeBST<T extends Comparable<T>> implements Iterable<T> {
 
 	@Override
 	public Iterator<T> iterator() {
+      int expectedModCount = this.modCount;
 		return new Iterator<T>() {
-
 			private final Queue<NodeBST> nodeList;
 
 			{
@@ -59,21 +65,32 @@ public class TreeNodeBST<T extends Comparable<T>> implements Iterable<T> {
 
 			@Override
 			public boolean hasNext() {
-				return !this.nodeList.isEmpty();
+			   boolean result = false;
+            if (expectedModCount == TreeNodeBST.this.modCount) {
+               result = !this.nodeList.isEmpty();
+            } else {
+               throw new ConcurrentModificationException();
+            }
+            return result;
 			}
 
 			@Override
 			public T next() {
 				NodeBST current = this.nodeList.poll();
 				T result = current.value;
-				if (current.left != null) {
-					this.nodeList.add(current.left);
-				}
-				if (current.right != null) {
-					this.nodeList.add(current.right);
-				}
-				return result;
+				if (expectedModCount == TreeNodeBST.this.modCount) {
+               if (current.left != null) {
+                  this.nodeList.add(current.left);
+               }
+               if (current.right != null) {
+                  this.nodeList.add(current.right);
+               }
+               return result;
+            } else {
+               throw new ConcurrentModificationException();
+            }
 			}
 		};
 	}
+
 }
