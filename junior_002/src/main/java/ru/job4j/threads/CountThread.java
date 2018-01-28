7cf52501064b5234be766945c2e606f1e068004e
Alexgta <9720560@gmail.com>
2018-01-28 16:59:18 +0300

    1. Cчитать количество слов и пробелов в тексте [#1016] - v2

--- junior_002/src/main/java/ru/job4j/threads/CountThread.java
+++ junior_002/src/main/java/ru/job4j/threads/CountThread.java
@@ -1,5 +1,7 @@
 package ru.job4j.threads;
 
+import ru.job4j.example.Usage;
+
 /**
  * Oracle Example
  * @author   A_Vasiliev
@@ -9,7 +11,7 @@ package ru.job4j.threads;
 
 
 public class CountThread {
-	static private String textString = "Cко ль ко про бе лов ? ";
+	static private String textString = "Cко льк о про бел ов? ";
 
 	public String getTextString() {
 		return this.textString;
@@ -22,7 +24,7 @@ public class CountThread {
 
 	static class SpaceCount implements Runnable {
 		private String text = textString;
-		private int count = 1;
+		private int count = 0;
 
 		public SpaceCount() {
 			this.text = CountThread.textString;
@@ -30,23 +32,21 @@ public class CountThread {
 
 		@Override
 		public void run() {
-			try {
-				for (int indx = 0; indx < text.length(); indx++) {
-					if (this.text.charAt(indx) == ' ') {
-						Thread.sleep(900);
-						// Print a message
-						System.out.printf("Пробелов: %s%n", count++);
-					}
+			for (int indx = 0; indx < text.length(); indx++) {
+				if (this.text.charAt(indx) == ' ') {
+					//Thread.sleep(900);
+					// Print a message
+					//System.out.printf("Пробелов: %s%n", count++);
+					count++;
 				}
-			} catch (InterruptedException e) {
-				e.printStackTrace();
 			}
+         System.out.printf("Пробелов: %s%n", count++);
 		}
 	}
 
 	static class LettersCount implements Runnable {
 		private String text = textString;
-		private int count = 1;
+		private int count = 0;
 
 		public LettersCount() {
 			this.text = textString;
@@ -54,27 +54,32 @@ public class CountThread {
 
 		@Override
 		public void run() {
-			try {
+			//try {
 				for (int indx = 0; indx < text.length(); indx++) {
 					if (this.text.charAt(indx) != ' ') {
-						Thread.sleep(5000);
+						//Thread.sleep(5000);
 						// Print a message
-						System.out.printf("Букв: %s%n", count++);
+						//System.out.printf("Букв: %s%n", count++);
+						count++;
 					}
 				}
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+			//} catch (InterruptedException e) {
+			//	e.printStackTrace();
+			//}
+         System.out.printf("Букв: %s%n", count++);
 		}
 	}
-
-	public static void main(String[] args) {
-		CountThread ct = new CountThread();
-		Thread tSpace = new Thread(new CountThread.SpaceCount());
+		
+	public void getAllCounts() {
+		Thread tSpace = new Thread(new SpaceCount());
 		tSpace.start();
-
-		Thread tLetters = new Thread(new CountThread.LettersCount());
-		tSpace.start();
+		Thread tLetters = new Thread(new LettersCount());
+		tLetters.start();
 	}
+
+	public static void main(String[] arg) {
+      CountThread ct = new CountThread();
+      ct.getAllCounts();
+   }
 
 }
