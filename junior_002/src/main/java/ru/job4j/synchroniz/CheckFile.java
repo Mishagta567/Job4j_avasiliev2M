d314a1bfc77e14b7b0e9bbff9da937fea7eb74ab
Alexgta <9720560@gmail.com>
2018-01-31 18:58:49 +0300

    4. Поиск текста в файле. [#1106] - v3

--- junior_002/src/main/java/ru/job4j/synchroniz/CheckFile.java
+++ junior_002/src/main/java/ru/job4j/synchroniz/CheckFile.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Queue;
 
 /**
- * Класс хранилища пользователей UserStorage [#1104]
+ * Поиск текста в файле. [#1106]
  * @author   A_Vasiliev
  * @since    16.01.2018
  * @version  1.0.0
@@ -22,27 +22,18 @@ public class CheckFile {
 		this.fileName = fileNm;
 	}
 
+	// checkFile возвращает true если в тексте есть искомая строка searchText
 	public boolean checkFile(String searchText) throws IOException {
 		String textLine;
 		boolean result = false;
-		String temp; // только для дебагинга
-
 		try {
-			// decleare a file
 			File file = new File(fileName);
-			// open a file
 			BufferedReader br = new BufferedReader(new FileReader(file));
-			// read a line
-
 			while ((textLine = br.readLine()) != null) {
-				temp = "-";
 				if (textLine.contains(searchText)) {
 					result = true;
 					break;
-				} else {
-					temp = "la la la";
 				}
-
 			}
 			br.close();
 		} catch (FileNotFoundException e) {
@@ -51,8 +42,7 @@ public class CheckFile {
 		} catch (IOException e) {
 			System.out.printf("Can't read line from frile %s", fileName);
 			e.printStackTrace();
-		} //finally {
-			// finish reading file;		}
+		}
 		return result;
 	}
 
