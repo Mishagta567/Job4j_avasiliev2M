31871ca67ba8df8dbd03e0c81e2001ae69510bff
Alexgta <9720560@gmail.com>
2018-02-13 23:47:57 +0300

    2. Удаление запрещенных слов [#859]

--- premiddle_003/src/main/java/ru/job4j/inout01/InputOutput.java
+++ premiddle_003/src/main/java/ru/job4j/inout01/InputOutput.java
@@ -1,7 +1,10 @@
 package ru.job4j.inout01;
 
 
+import sun.misc.IOUtils;
+
 import java.io.*;
+import java.nio.charset.Charset;
 
 
 /**
@@ -13,7 +16,6 @@ import java.io.*;
 public class InputOutput {
 
    /**
-    *
     * @param in - input Stream int
     */
    public boolean checkNumberFromStream(InputStream in) {
@@ -48,21 +50,100 @@ public class InputOutput {
       return result;
    }
 
+   /**
+    * @param in - input Stream int
+    * @param out - sure thing outPutStream
+    * @param abuse - all abuse words.
+    */
+   void dropAbuses(InputStream in, OutputStream out, String[] abuse) throws IOException {
+      byte[] buff = new byte[1024];
+      //BufferedInputStream - good for speed
+      String stringFromBuff = "";
+      String uncheckedString = "";
+      String checkedString = "";
+      int count = 0;
+      int spacePosition = 0;
+
+      int i;
+      while ((i = in.read()) != -1) {
+         //out.write(i);
+         //baos.flush();
+         stringFromBuff = stringFromBuff + (char) i;
+         count++;
+
+         if (count > 1024) {
+            // lets move back to first ' ' char from back
+            if (!uncheckedString.equals("")) {
+               checkedString = uncheckedString + checkedString;
+               uncheckedString = "";
+            }
+
+            spacePosition = stringFromBuff.lastIndexOf(' ');
+            if (spacePosition > 0) {
+               if (uncheckedString.equals(null)) {
+                  checkedString = stringFromBuff.substring(0, spacePosition);
+                  uncheckedString = stringFromBuff.substring(spacePosition);
+               } else {
+                  checkedString = uncheckedString + stringFromBuff.substring(0, spacePosition);
+                  uncheckedString = stringFromBuff.substring(spacePosition);
+               }
+            }
+            count = 0;
+            checkedString = dropAbuse(checkedString, abuse);
+            out.write(checkedString.getBytes());
+            checkedString = "";
+         }
+      }
+      if (!stringFromBuff.equals("")) {
+         if (!uncheckedString.equals("")) {
+            stringFromBuff = uncheckedString + stringFromBuff;
+         }
+         checkedString = dropAbuse(stringFromBuff, abuse);
+         out.write(checkedString.getBytes());
+         checkedString = "";
+      }
+
+   }
+
+   private String dropAbuse(String checkString, String[] abuse) {
+      String resultString = checkString;
+      int abusePosition = 0;
+      // Now, lets check for Abouse words
+      for (String abs : abuse) {
+         while ((abusePosition = resultString.indexOf(abs)) > 0) {
+            // and remove them if find any
+            resultString = resultString.substring(0, abusePosition)
+                    + resultString.substring(abusePosition + abs.length());
+         }
+      }
+      return resultString;
+   }
+
+
 
    /**
     * @param args - I don't use it yet
     */
-   public static void main(final String[] args) {
-      InputOutput iot = new InputOutput();
+   public static void main(String[] args) throws IOException {
 
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      InputStreamReader in = new InputStreamReader(System.in);
-      int number = 0;
+      System.out.print("Hello there");
 
-      // ByteArrayInputStream(new byte[] {49, 50, 53, 54} - 1256
-      System.out.println(iot.checkNumberFromStream(new ByteArrayInputStream(new byte[] {49, 50, 53, 54})));
    }
 
+   //Reader reader = new InputStreamReader(in, "UTF-8");
+   //System.out.println(out.toString());
+   //ByteArrayInputStream bais = new ByteArrayInputStream(startString.getBytes("UTF-8"));
+   //ByteArrayOutputStream baos = new ByteArrayOutputStream();
+   //int i;
+   //String myString = "";
+   //while ( ( i = in.read() ) != -1  ){
+   //   //baos.write(i);
+   //   //baos.flush();
+   //   myString = myString + (char) i;
+   //}
+
+   // 12-3-29:55 writeUTF,  38:53 - dataInput.readUTF
+   // Reader reader = new InputStreamReader(in, "windows-1251")  13-1-14:53
    //Scanner sc = new Scanner(System.in);
    //String str1;
    //String str2;
@@ -83,4 +164,6 @@ public class InputOutput {
    // byte(125) -> int(125)
    // byte(126) -> int(126)
    // byte(127) -> int(127)
+   // min 18, 20
+   // Exeption: 1-4-26.18
 }
--- premiddle_003/src/test/java/ru/job4j/inout01/InOutTestTest.java
+++ premiddle_003/src/test/java/ru/job4j/inout01/InOutTestTest.java
@@ -5,22 +5,41 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class InOutTestTest {
 
    @Test
    public void inputStreamGiveEvenNumber() throws Exception {
       InputOutput iot = new InputOutput();
+      String string = "1256";
       // new byte[] {49, 50, 53, 54}) = 1256
-      boolean result = iot.checkNumberFromStream(new ByteArrayInputStream(new byte[] {49, 50, 53, 54}));
+      boolean result = iot.checkNumberFromStream(new ByteArrayInputStream(string.getBytes())); //new byte[] {49, 50, 53, 54}));
       Assert.assertThat(result, Is.is(true));
    }
 
    @Test
    public void inputStreamGiveNotEvenNumber() throws Exception {
       InputOutput iot = new InputOutput();
+      String string = "1257";
       // new byte[] {49, 50, 53, 55}) = 1257
-      boolean result = iot.checkNumberFromStream(new ByteArrayInputStream(new byte[] {49, 50, 53, 55}));
+      boolean result = iot.checkNumberFromStream(new ByteArrayInputStream(string.getBytes())); // new byte[] {49, 50, 53, 55}));
       Assert.assertThat(result, Is.is(false));
    }
+
+   @Test
+   public void checkRemoveAbuse() throws Exception {
+      String[] abuse = {"WWW", "XXX", "@"};
+      InputOutput iot = new InputOutput();
+      String startString = "In WWW too much XXX and fishing. We have to hide our @";
+      InputStream in = new ByteArrayInputStream(startString.getBytes("UTF-8"));
+
+      OutputStream out = new ByteArrayOutputStream();
+      iot.dropAbuses(in, out, abuse);
+      String expectResult = "In  too much  and fishing. We have to hide our ";
+      Assert.assertThat(out.toString(), Is.is(expectResult));
+   }
+
 }
