353edb61ba4716cdb283bce24ed6b470a1763a93
Alexgta <9720560@gmail.com>
2018-02-23 14:52:43 +0300

    Мелкие правки

--- junior_004/src/main/java/ru/job4j/mvc05/UsersController.java
+++ junior_004/src/main/java/ru/job4j/mvc05/UsersController.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
@@ -32,20 +33,41 @@ public class UsersController extends HttpServlet {
     */
    private final UserStore userStore = UserStore.SingletonEnum.INSTANCE.getInstance();
 
-
-   /**
-    * Returns user-info specified by login.
-    */
+   // Копия из junior_004b до правки.
    @Override
-   public void doGet(HttpServletRequest req, HttpServletResponse resp)
-           throws ServletException, IOException {
-      PrintWriter writer = new PrintWriter(resp.getOutputStream());
-      writer.append("Hello there. Page src/main/java/mvc05/UsersController.java");
-      writer.flush();
-      // По какой-то причине не работает переадресация.
-      //req.getRequestDispatcher("/WEB-INF/views/UsersView.jsp").forward(req, resp);
-      //req.getRequestDispatcher("/UsersView.jsp").forward(req, resp);
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      HttpSession session = req.getSession();
+      synchronized (session) {
+         if (session == null || session.getAttribute("login") == null) {
+            resp.sendRedirect(String.format("%s/signin?out=UserController_doGet"));
+         } else {
+
+            req.getRequestDispatcher("/WEB-INF/mvc05/userList.jsp?out=SigninController_doGet").forward(req, resp);
+         }
+      }
    }
 
+   @Override
+   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      resp.setContentType("text/html");
+      String login = req.getParameter("login");
+      String pass = req.getParameter("password");
+      try {
+         if (userStore.isCredentional(login, pass)) {
+            HttpSession session = req.getSession();
+            synchronized (session) {
+               session.setAttribute("role", userStore.getRole(login));
+               session.setAttribute("login", login);
+            }
+            resp.sendRedirect(("/WEB-INF/mvc05/userList.jsp?out=SigninController_Signed"));
+         } else {
+            req.setAttribute("error", "Credentional invalid");
+            resp.sendRedirect(("/items/LoginView.jsp?error=Invalid"));
+            //doGet(req, resp);
+         }
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }
+   }
 
 }
